28477fed2672f51ebe28e0c1bc9503adecb56627
Piotr Choiński <89218@student.pb.edu.pl>
2026-04-02 09:45:02 +0200

    Dodano Widoki do Tworzenia tasków i do wyświetlania szczegółowych informacji o tasku

---
+++ TeamTaskManager/Views/CreateTaskWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace TeamTaskManager.Views
+{
+    public partial class CreateTaskWindow : Window
+    {
+        public CreateTaskWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+          
+            if (string.IsNullOrWhiteSpace(TitleTextBox.Text))
+            {
+                MessageBox.Show("Nazwa zadania jest wymagana!", "Błąd walidacji", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            
+
+            MessageBox.Show($"Utworzono zadanie: {TitleTextBox.Text}", "Sukces", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            this.Close();
+        }
+    }
+}

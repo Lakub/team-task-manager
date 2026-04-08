56bef48f145ec3a523f0ed8d61e2120bda9969b0
Piotr Choiński <89218@student.pb.edu.pl>
2026-04-08 18:17:32 +0200

    Dodanie widoków dla modułu Dokumentacji

---
+++ TeamTaskManager/Views/ArticleEditWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace TeamTaskManager.Views
+{
+    public partial class ArticleEditWindow : Window
+    {
+        public ArticleEditWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TitleTextBox.Text))
+            {
+                MessageBox.Show("Tytuł strony jest wymagany!", "Błąd walidacji", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            MessageBox.Show($"Zapisano stronę: {TitleTextBox.Text}", "Sukces", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
+        }
+    }
+}
---
+++ TeamTaskManager/Views/WikiMainView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeamTaskManager.Views
+{
+    public partial class WikiMainView : UserControl
+    {
+        public WikiMainView()
+        {
+            InitializeComponent();
+        }
+
+        private void NewArticleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ArticleEditWindow editWindow = new ArticleEditWindow();
+            editWindow.ShowDialog();
+        }
+
+        private void EditArticleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ArticleEditWindow editWindow = new ArticleEditWindow();
+            // Tutaj w przyszłości przekażesz obiekt artykułu do edycji
+            editWindow.ShowDialog();
+        }
+    }
+}

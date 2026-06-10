d7b1e908a3fce676c955db955481b45f974b755a
Korellek <korelleks@gmail.com>
2026-06-10 20:31:31 +0200

    fix permów tworzenia sprintow

--- TeamTaskManager/Services/ProjectService.cs
+++ TeamTaskManager/Services/ProjectService.cs
@@ -36,6 +36,7 @@ namespace TeamTaskManager.Services
         public async Task<(Project Project, List<Sprint> Sprints)> GetSprintsByProjectIdAsync(int projectId)
         {
             var project = await _context.Projects
+                .Include(p => p.ProjectUsers)
                 .FirstOrDefaultAsync(p => p.Id == projectId);
 
             var sprints = await _context.Sprints
